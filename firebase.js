f20ddc072f2754842683c5ce992edc8dcf4979b1
warrior1112 <42972418+warrior1112@users.noreply.github.com>
2020-03-21 15:39:30 +0530

    captured image uploading into firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDk8qXUaj6a3qfQkR4yTgG2bQl_NSwH7IA",
+    authDomain: "digitalstudentmanagement.firebaseapp.com",
+    databaseURL: "https://digitalstudentmanagement.firebaseio.com",
+    projectId: "digitalstudentmanagement",
+    storageBucket: "digitalstudentmanagement.appspot.com",
+    messagingSenderId: "148815692926",
+    appId: "1:148815692926:web:601ea15a02607e0279c42f",
+    measurementId: "G-NJLWSE27BN"
+  }; 
